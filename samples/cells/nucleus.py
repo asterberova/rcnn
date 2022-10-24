6d47b97f34fba121468f1c9d46c7f647d1e89b54
Adéla Šterberová <asterberova@gmail.com>
2022-10-24 15:25:09 +0200

    masks fix

--- samples/cells/nucleus.py
+++ samples/cells/nucleus.py
@@ -412,7 +412,11 @@ def detect(model, dataset_dir, subset):
             print("=================================================")
             print(mask)
             unmolded_mask = utils.unmold_mask(mask, roi, image.shape)
-            cv2.imwrite('{}/{}/masks/{}.png'.format(submit_dir, dataset.image_info[image_id]["id"], str(id_mask)), unmolded_mask)
+            print("=================================================")
+            print(unmolded_mask)
+            # cv2.imwrite('{}/{}/masks/{}.png'.format(submit_dir, dataset.image_info[image_id]["id"], str(id_mask)), unmolded_mask)
+            visualize.display_images(unmolded_mask)
+            plt.savefig('{}/{}/masks/{}.png'.format(submit_dir, dataset.image_info[image_id]["id"], str(id_mask)))
             id_mask += 1
 
         # calculate statistics, including AP

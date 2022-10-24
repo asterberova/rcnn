536ac745201dc4ac9d8dba3a23e3a84e17b60e44
Adéla Šterberová <asterberova@gmail.com>
2022-10-24 22:48:08 +0200

    masks fix

--- samples/cells/nucleus.py
+++ samples/cells/nucleus.py
@@ -372,6 +372,8 @@ def detect(model, dataset_dir, subset):
         image = dataset.load_image(image_id)
         # Detect objects
         r = model.detect([image], verbose=0)[0]
+        print(r)
+        print('=======================================================================================')
         # Encode image to RLE. Returns a string of multiple lines
         source_id = dataset.image_info[image_id]["id"]
         rle = mask_to_rle(source_id, r["masks"], r["scores"])

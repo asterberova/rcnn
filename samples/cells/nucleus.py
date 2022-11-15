7039baa3d22fc8ff47ba1a6114ccdb7d89ed22c2
Adéla Šterberová <asterberova@gmail.com>
2022-11-15 16:32:32 +0100

    shape of detection doesn't match the original image, no reshaping images when loading

--- samples/cells/nucleus.py
+++ samples/cells/nucleus.py
@@ -101,7 +101,7 @@ class CellsConfig(Config):
 
     # Input image resizing
     # Random crops of size 512x512
-    IMAGE_RESIZE_MODE = "crop"
+    IMAGE_RESIZE_MODE = "none"
     IMAGE_MIN_DIM = 512
     IMAGE_MAX_DIM = 512
     IMAGE_MIN_SCALE = 2.0
@@ -147,7 +147,7 @@ class CellsInferenceConfig(CellsConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
     # Don't resize imager for inferencing
-    IMAGE_RESIZE_MODE = "pad64"
+    IMAGE_RESIZE_MODE = "none"
     # Non-max suppression threshold to filter RPN proposals.
     # You can increase this during training to generate more propsals.
     RPN_NMS_THRESHOLD = 0.7

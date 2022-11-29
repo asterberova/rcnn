37f66c83371543beee5f75efca5ff48a8f644165
Adéla Šterberová <asterberova@gmail.com>
2022-11-29 20:41:58 +0100

    resize mode back to pad64 in cell interference

--- samples/cells/nucleus.py
+++ samples/cells/nucleus.py
@@ -147,7 +147,8 @@ class CellsInferenceConfig(CellsConfig):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
     # Don't resize imager for inferencing
-    IMAGE_RESIZE_MODE = "none"
+    # pad64/crop/square/none
+    IMAGE_RESIZE_MODE = "pad64"
     # Non-max suppression threshold to filter RPN proposals.
     # You can increase this during training to generate more propsals.
     RPN_NMS_THRESHOLD = 0.7

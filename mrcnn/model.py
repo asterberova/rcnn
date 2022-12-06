1bd04c8ca92346d141777561fd26654a14a7029b
Adéla Šterberová <asterberova@gmail.com>
2022-12-06 09:25:26 +0100

    num_workers = multiprocessing.cpu_count()

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2363,7 +2363,7 @@ class MaskRCNN(object):
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers/2,
+            workers=workers,
             use_multiprocessing=workers > 1,
         )
         self.epoch = max(self.epoch, epochs)
